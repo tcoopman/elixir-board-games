d01f7b2054703a7924fb32eb60ab9a4d257d6f75
Thomas Coopman <thomas.coopman@gmail.com>
2020-08-17 15:03:46 +0200

    joining a game

--- lib/board_games/tempel_des_schreckens/tempel_des_schreckens.ex
+++ lib/board_games/tempel_des_schreckens/tempel_des_schreckens.ex
@@ -24,7 +24,10 @@ defmodule BoardGames.TempelDesSchreckens do
   def execute(game, %BoardGames.TempelDesSchreckens.Command.JoinGame{player_id: player_id}),
     do: join_game(game, player_id)
 
-  @spec apply(BoardGames.TempelDesSchreckens.t(), BoardGames.TempelDesSchreckens.Event.GameCreated.t()) ::
+  @spec apply(
+          BoardGames.TempelDesSchreckens.t(),
+          BoardGames.TempelDesSchreckens.Event.GameCreated.t()
+        ) ::
           BoardGames.TempelDesSchreckens.t()
   def apply(%TempelDesSchreckens{} = game, %BoardGames.TempelDesSchreckens.Event.GameCreated{
         game_id: game_id,
@@ -33,27 +36,37 @@ defmodule BoardGames.TempelDesSchreckens do
     %{game | game_id: game_id, name: name}
   end
 
-  def apply(%TempelDesSchreckens{} = game, %BoardGames.TempelDesSchreckens.Event.JoinedGame{
-        player_id: player_id
-      }) do
-    %{game | players: [player_id]}
+  def apply(
+        %TempelDesSchreckens{players: players} = game,
+        %BoardGames.TempelDesSchreckens.Event.JoinedGame{
+          player_id: player_id
+        }
+      ) do
+    %{game | players: [player_id | players]}
   end
 
-  defp create_game(%TempelDesSchreckens{players: []}, id, name) when is_non_empty_string?(name) do
+  defp create_game(%TempelDesSchreckens{}, _, name) when not is_non_empty_string?(name),
+    do: {:error, :invalid_name}
+
+  defp create_game(%TempelDesSchreckens{players: []}, id, name) do
     %BoardGames.TempelDesSchreckens.Event.GameCreated{
       game_id: id,
       name: name
     }
   end
 
-  defp create_game(_, _, _), do: {:error, :invalid_name}
+  defp join_game(%TempelDesSchreckens{game_id: game_id}, _)
+       when not is_non_empty_string?(game_id),
+       do: {:error, :game_is_not_in_progress}
 
-  defp join_game(%TempelDesSchreckens{game_id: game_id} = game, player_id) when is_non_empty_string?(game_id) do
+  defp join_game(%TempelDesSchreckens{players: players}, _)
+       when length(players) >= 10,
+       do: {:error, :max_number_of_players_reached}
+
+  defp join_game(%TempelDesSchreckens{} = game, player_id) do
     %BoardGames.TempelDesSchreckens.Event.JoinedGame{
       game_id: game.game_id,
       player_id: player_id
     }
   end
-
-  defp join_game(_, _), do: {:error, :game_is_not_in_progress}
 end
--- test/board_games/tempel_des_schreckens/tempel_des_schreckens_test.exs
+++ test/board_games/tempel_des_schreckens/tempel_des_schreckens_test.exs
@@ -52,5 +52,72 @@ defmodule BoardGames.TempelDesSchreckensTest do
 
       assert_error(command, {:error, :game_is_not_in_progress})
     end
+
+    test "that is waiting for people" do
+      game_id = "game id"
+      player_id = "player 1"
+
+      assert_events(
+        [
+          %Event.GameCreated{
+            game_id: game_id,
+            name: "some game"
+          }
+        ],
+        %Command.JoinGame{
+          player_id: player_id,
+          game_id: game_id
+        },
+        %Event.JoinedGame{
+          game_id: game_id,
+          player_id: player_id
+        }
+      )
+    end
+
+    test "that has the 9 players" do
+      game_id = "game id"
+      player_id = "player 10"
+
+      current_players = for i <- 1..9, do: %Event.JoinedGame{game_id: game_id, player_id: "player_#{i}"}
+
+      assert_events(
+        [
+          %Event.GameCreated{
+            game_id: game_id,
+            name: "some game"
+          }
+        ] ++ current_players,
+        %Command.JoinGame{
+          player_id: player_id,
+          game_id: game_id
+        },
+        %Event.JoinedGame{
+          game_id: game_id,
+          player_id: player_id
+        }
+      )
+    end
+
+    test "that has the maximum of players already" do
+      game_id = "game id"
+      player_id = "player 10"
+
+      current_players = for i <- 1..10, do: %Event.JoinedGame{game_id: game_id, player_id: "player_#{i}"}
+
+      assert_error(
+        [
+          %Event.GameCreated{
+            game_id: game_id,
+            name: "some game"
+          }
+        ] ++ current_players,
+        %Command.JoinGame{
+          player_id: player_id,
+          game_id: game_id
+        },
+        {:error, :max_number_of_players_reached}
+      )
+    end
   end
 end
