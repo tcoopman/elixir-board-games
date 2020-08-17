d592b9967a9ec536d582947973e087c3cd5ad715
Thomas Coopman <thomas.coopman@gmail.com>
2020-08-17 11:27:57 +0200

    refactor: use an alias

--- test/board_games/tempel_des_schreckens/tempel_des_schreckens_test.exs
+++ test/board_games/tempel_des_schreckens/tempel_des_schreckens_test.exs
@@ -4,42 +4,53 @@ defmodule BoardGames.TempelDesSchreckensTest do
 
   import Commanded.Assertions.EventAssertions
 
+  alias BoardGames.TempelDesSchreckens.{Command, Event}
+
   test "Create a new game" do
     :ok =
-      BoardGames.App.dispatch(%BoardGames.TempelDesSchreckens.Command.CreateGame{
+      BoardGames.App.dispatch(%Command.CreateGame{
         player_id: "player_id",
         game_id: "game_id",
-        name: "the game name",
+        name: "the game name"
       })
 
-    assert_receive_event(BoardGames.App, BoardGames.TempelDesSchreckens.Event.GameCreated, fn event ->
-      assert event.game_id == "game_id"
-      assert event.name == "the game name"
-    end)
-    assert_receive_event(BoardGames.App, BoardGames.TempelDesSchreckens.Event.JoinedGame, fn event ->
-      assert event.game_id == "game_id"
-      assert event.player_id == "player_id"
-    end)
+    assert_receive_event(
+      BoardGames.App,
+      Event.GameCreated,
+      fn event ->
+        assert event.game_id == "game_id"
+        assert event.name == "the game name"
+      end
+    )
+
+    assert_receive_event(
+      BoardGames.App,
+      Event.JoinedGame,
+      fn event ->
+        assert event.game_id == "game_id"
+        assert event.player_id == "player_id"
+      end
+    )
   end
 
   test "Try to create a game with an empty name" do
-    command = %BoardGames.TempelDesSchreckens.Command.CreateGame{
-        player_id: "player_id",
-        game_id: "game_id",
-        name: "",
-      }
+    command = %Command.CreateGame{
+      player_id: "player_id",
+      game_id: "game_id",
+      name: ""
+    }
 
     assert_error(command, {:error, :invalid_name})
   end
 
   describe "Join a game" do
     test "that is not yet created" do
-    command = %BoardGames.TempelDesSchreckens.Command.JoinGame{
+      command = %Command.JoinGame{
         player_id: "player_id",
-        game_id: "game_id",
+        game_id: "game_id"
       }
 
-    assert_error(command, {:error, :game_is_not_in_progress})
+      assert_error(command, {:error, :game_is_not_in_progress})
     end
   end
 end
