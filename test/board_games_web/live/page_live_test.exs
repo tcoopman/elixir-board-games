5e127d53de5f76bd9189a272063dcd41bef4b192
Thomas Coopman <thomas.coopman@gmail.com>
2020-08-18 15:33:31 +0200

    fix the live test

--- test/board_games_web/live/page_live_test.exs
+++ test/board_games_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule BoardGamesWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "Create a new game"
+    assert render(page_live) =~ "Create a new game"
   end
 end
