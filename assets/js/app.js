869522cd3ed3b05b1cb2ea4f0de480f230fe3361
Thomas Coopman <thomas.coopman@gmail.com>
2020-08-21 16:58:30 +0200

    add animations to flash with alpinejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,9 +16,32 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
+import "alpinejs"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let Hooks = {}
+Hooks.Flash = {
+  mounted() {
+    this.el.addEventListener('transitionend', (evt) => {
+      evt.stopPropagation();
+      const result = /flash-([\w]*)/.exec(evt.target.id);
+      if (result) {
+        const [_, key] = result
+        this.pushEvent("lv:clear-flash", {key: key})
+      }
+    })
+  }
+};
+
+let liveSocket = new LiveSocket("/live", Socket, {
+  dom: {
+    onBeforeElUpdated(from, to){
+      if(from.__x){ window.Alpine.clone(from.__x, to) }
+    }
+  },
+  params: {_csrf_token: csrfToken},
+  hooks: Hooks
+})
 
 // Show progress bar on live navigation and form submits
 window.addEventListener("phx:page-loading-start", info => NProgress.start())
